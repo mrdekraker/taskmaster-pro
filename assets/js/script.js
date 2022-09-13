bac6c9135ef73fda855ada68e3df5824bfe8d192
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-09-13 17:17:10 -0500

    added due date handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-console */
-/* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
 let tasks = {};
 
 const createTask = function (taskText, taskDate, taskList) {
